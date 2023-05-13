2bd439b82786ead7aafb01c3412413d067142533
Marco Stecca <marco.stecca99@gmail.com>
2023-05-13 20:48:59 +0200

    Split the function The creation of the handler is given to _gethandler logging._StreamT does not exist at runtime

--- src/logging/logger.py
+++ src/logging/logger.py
@@ -1,8 +1,15 @@
 import logging
 from sys import stdout
-from typing import Iterable, TypeAlias
+from typing import Sequence, TypeAlias, TypeVar, Protocol
 
 fname: TypeAlias = str
+_T_contra = TypeVar("_T_contra", contravariant=True)
+
+
+class SupportsWrite(Protocol[_T_contra]):
+    def write(self, __s: _T_contra) -> object:
+        ...
+
 
 LOG_LEVELS = (
     logging.DEBUG,
@@ -11,35 +18,46 @@ LOG_LEVELS = (
     logging.ERROR,
     logging.CRITICAL,
 )
-LOG_CODENAMES = ("DBG", "INF", "WARN", "ERR", "CRT")
+LOG_CODENAMES = ("DBG", "INF", "WRN", "ERR", "CRT")
+
+
+def _gethandler(
+    out: fname | SupportsWrite, level: int, formatter: logging.Formatter
+) -> logging.FileHandler | logging.StreamHandler:
+    if isinstance(out, str):
+        hand = logging.FileHandler(out, "a+")
+    else:
+        hand = logging.StreamHandler(out)
+    hand.setFormatter(formatter)
+    hand.setLevel(level)
+    return hand
 
 
 def getlogger(
-    name: str,
-    outputs: Iterable[fname | logging._StreamT],
+    loggername: str,
+    outputs: Sequence[fname | SupportsWrite] | fname | SupportsWrite,
     level: int = logging.INFO,
-    codenames: Iterable[str] = LOG_CODENAMES,
+    codenames: Sequence[str] = LOG_CODENAMES,
 ) -> logging.Logger:
     for type, name in zip(LOG_LEVELS, codenames):
         logging.addLevelName(type, name)
 
-    log = logging.getLogger(name)
+    log = logging.getLogger(loggername)
     log.setLevel(level)
 
     logformatter = logging.Formatter(
-        "[%(levelname)s] %(asctime)s %(name)s : %(message)s", "%Y-%m-%d %H:%M:%S"
+        "[%(levelname)s] %(asctime)s <%(name)s> : %(message)s", "%Y-%m-%d %H:%M:%S"
     )
 
-    for out in outputs:
-        if isinstance(out, str):
-            filehandler = logging.FileHandler(out, "a+")
-            filehandler.setFormatter(logformatter)
-            filehandler.setLevel(logging.DEBUG)
-            log.addHandler(filehandler)
-        else:
-            printhandler = logging.StreamHandler(stdout)
-            printhandler.setFormatter(logformatter)
-            printhandler.setLevel(logging.DEBUG)
-            log.addHandler(printhandler)
+    if isinstance(outputs, str):
+        handler = _gethandler(outputs, level, logformatter)
+        log.addHandler(handler)
+    elif isinstance(outputs, Sequence):
+        for out in outputs:
+            handler = _gethandler(out, level, logformatter)
+            log.addHandler(handler)
+    else:
+        handler = _gethandler(outputs, level, logformatter)
+        log.addHandler(handler)
 
     return log
